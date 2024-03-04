fa6035cf2ce6f621831beb8a175b1d20ee5198c4
DariaLuca <andreeadarialuca@gmail.com>
2024-03-04 20:04:08 +0200

    Add modulo example

--- script.js
+++ script.js
@@ -38,3 +38,8 @@ let numberD = 10;
 let divisionResult = numberC / numberD;
 console.log(`${numberC} impartit la ${numberD} rezulta ${divisionResult}`);
 
+//Restul impartirii
+let numberE = 15; 
+let numberF = 4;
+let moduloResult = numberE % numberF;
+console.log(`Result impartirii lui ${numberE} la ${numberF} este ${moduloResult}`);
